243870d9b492b56e29ab56c9cfa56bd37b09d5c8
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2024-12-29 05:26:25 -0600

    Update settingsPage.py

--- src/settingsPage.py
+++ src/settingsPage.py
@@ -12,3 +12,12 @@ class Settings(QWidget):
         self.settingsWidget = QWidget()
         self.settingsLayout = QVBoxLayout()
         self.setLayout(self.settingsLayout)
+        #TODO: Do this after figuring out the scanner. 
+        # need to decide if I want to process individual folders or scan through the entire skyrim directory.
+        self.modsFolderPathLayout = QHBoxLayout()
+        self.outputFolderPathLayout = QHBoxLayout()
+        self.updateHeaderLayout = QHBoxLayout()
+        self.showPluginsWithCellsLayout = QHBoxLayout()
+        self.singleOutputModFolderLayout = QHBoxLayout()
+        self.exclusionsLayout = QHBoxLayout()
+
