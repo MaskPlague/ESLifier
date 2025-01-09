771c30f6c925ac1f4ebed309f744af74218a092f
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-01-09 03:50:25 -0600

    get bsa list and add it to the dependency_dictionary

--- src/dependency_getter.py
+++ src/dependency_getter.py
@@ -3,10 +3,13 @@ import re
 import json
 
 class dependecy_getter():
+    bsa_list = []
     def scan(path):
+        dependecy_getter.bsa_list = []
         dependecy_getter.dependency_dictionary = {}
         dependecy_getter.plugins = dependecy_getter.get_list_of_plugins(path)
         dependecy_getter.create_dependency_dictionary()
+        dependecy_getter.dependency_dictionary['BSA_list'] = dependecy_getter.bsa_list
         dependecy_getter.dump_to_file("ESLifier_Data/dependency_dictionary.json")
         return dependecy_getter.dependency_dictionary
 
@@ -14,8 +17,10 @@ class dependecy_getter():
         plugins = []
         for root, dirs, files in os.walk(path):
             for file in files:
-                if re.search(r'\.es[pml]$', file):
+                if re.search(r'\.es[pml]$', file) and not '\\optional' in root.lower():
                     plugins.append(os.path.join(root,file))
+                elif '.bsa' in file.lower():
+                    dependecy_getter.bsa_list.append(file.lower())
         return plugins
     
     def dump_to_file(file):
@@ -32,11 +37,11 @@ class dependecy_getter():
         plugin_names = []
         for plugin in dependecy_getter.plugins:
             plugin_names.append(os.path.basename(plugin).lower())
-            plugin_names.append(os.path.basename(plugin).lower() + '_path')
+            #plugin_names.append(os.path.basename(plugin).lower() + '_path')
 
         dependecy_getter.dependency_dictionary = {plugin: [] for plugin in plugin_names}
         for plugin in dependecy_getter.plugins:
-            dependecy_getter.dependency_dictionary[os.path.basename(plugin).lower() + '_path'] = plugin
+            #dependecy_getter.dependency_dictionary[os.path.basename(plugin).lower() + '_path'] = plugin
             masters = dependecy_getter.getMasters(plugin)
             if len(masters) > 0:
                 for master in masters:
