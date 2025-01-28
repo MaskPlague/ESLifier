4919841ae6b5af9592edb3c1ebad59e5a7aafc14
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-01-27 22:33:49 -0600

    remove get_list_of_plugins()
    remove get_lost_of_plugins() as the job has been moved to scanner.py as it is always called before dependency_getter.scan() and os.walk() is already performed there once.

--- src/dependency_getter.py
+++ src/dependency_getter.py
@@ -5,32 +5,22 @@ import json
 class dependecy_getter():
     bsa_list = []
     def scan(path):
-        dependecy_getter.bsa_list = []
         dependecy_getter.dependency_dictionary = {}
-        dependecy_getter.plugins = dependecy_getter.get_list_of_plugins(path)
+        dependecy_getter.plugins = dependecy_getter.get_from_file("ESLifier_Data/plugin_list.json")
         dependecy_getter.create_dependency_dictionary()
-        dependecy_getter.dependency_dictionary['BSA_list'] = dependecy_getter.bsa_list
         dependecy_getter.dump_to_file("ESLifier_Data/dependency_dictionary.json")
         return dependecy_getter.dependency_dictionary
-
-    def get_list_of_plugins(path):
-        plugins = []
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                if re.search(r'\.es[pml]$', file) and not '\\optional' in root.lower():
-                    plugins.append(os.path.join(root,file))
-                elif '.bsa' in file.lower():
-                    dependecy_getter.bsa_list.append(file.lower())
-        return plugins
     
     def dump_to_file(file):
         with open(file, 'w+', encoding='utf-8') as f:
             json.dump(dependecy_getter.dependency_dictionary, f, ensure_ascii=False, indent=4)
     
     def get_from_file(file):
-        data = {}
-        with open(file, 'r') as f:
-            data = json.load(f)
+        try:
+            with open(file, 'r') as f:
+                data = json.load(f)
+        except:
+            data = []
         return data
     
     def create_dependency_dictionary():
