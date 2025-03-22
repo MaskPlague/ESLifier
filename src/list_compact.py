0ab635fae5e11efba8ffed931f4edd5536b5d621
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-03-22 04:45:18 -0500

    Add WRLD flags change to constants for columns
    constants for columns allows for easier adding of new flags in the future.
    changed the way filter/hiding rows works

--- src/list_compact.py
+++ src/list_compact.py
@@ -9,23 +9,32 @@ from blacklist import blacklist
 class list_compactable(QTableWidget):
     def __init__(self):
         super().__init__()
-        self.setColumnCount(6)
-        self.setHorizontalHeaderLabels(['*   Mod', 'CELL Records', 'SKSE DLL', 'Dependencies', '', 'Hider'])
-        self.horizontalHeaderItem(0).setToolTip('This is the plugin name. Select which plugins you wish to compact.')
-        self.horizontalHeaderItem(1).setToolTip('This is the CELL Record Flag. If an ESL plugin creates a new CELL\n'+
+        self.COL_COUNT = 7
+        self.MOD_COL = 0
+        self.CELL_COL = 1
+        self.WRLD_COL = 2
+        self.SKSE_COL = 3
+        self.DEP_COL = 4
+        self.DEP_DISP_COL = 5
+        self.HIDER_COL = 6
+        self.setColumnCount(self.COL_COUNT)
+        self.setHorizontalHeaderLabels(['*   Mod', 'CELL Records', 'WRLD Records', 'SKSE DLL', 'Dependencies', '', 'Hider'])
+        self.horizontalHeaderItem(self.MOD_COL).setToolTip('This is the plugin name. Select which plugins you wish to compact.')
+        self.horizontalHeaderItem(self.CELL_COL).setToolTip('This is the CELL Record Flag. If an ESL plugin creates a new CELL\n'+
                                                 'and another mod changes that CELL then it may not work due to an engine bug.\n'+
                                                 'If an ESL plugin creates a new interior CELL then that cell may experience\n'+
                                                 'issues when reloading a save without restarting the game.\n'+
                                                 '"New  CELL" indicates the presence of a new CELL record.\n'+
                                                 '"!New Interior CELL!" indicates that a new CELL is an interior.\n'+
                                                 '"!!New CELL Changed!!" indicates that a new CELL record is changed by a dependent plugin.')
-        #TODO: Add wrld space flags to both lists
-        self.horizontalHeaderItem(2).setToolTip('This is the skse DLL flag. If a dll has the plugin name in it then it may\n'+
+        self.horizontalHeaderItem(self.WRLD_COL).setToolTip('This is the WRLD Record Flag. If an plugin is flagged ESL\n'+
+                                                       'then the new worldspace may have landscape issues (no ground).')
+        self.horizontalHeaderItem(self.SKSE_COL).setToolTip('This is the skse DLL flag. If a dll has the plugin name in it then it may\n'+
                                                 'have a LookUpForm() call that may break after compacting a flagged plugin.')
-        self.horizontalHeaderItem(3).setToolTip('If a plugin has other plugins with it as a master, they will appear when\n'+
+        self.horizontalHeaderItem(self.DEP_COL).setToolTip('If a plugin has other plugins with it as a master, they will appear when\n'+
                                                 'the button is clicked. These will also have their Form IDs patched to\n'+
                                                 'reflect the Master plugin\'s changes.')
-        self.setColumnHidden(5, True)
+        self.setColumnHidden(self.HIDER_COL, True)
         self.horizontalHeader().sortIndicatorChanged.connect(self.hide_rows)
         self.verticalHeader().setHidden(True)
         self.setShowGrid(False)
@@ -38,9 +47,11 @@ class list_compactable(QTableWidget):
         self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
         self.customContextMenuRequested.connect(self.contextMenu)
         self.flag_dict = {}
+        self.show_cells = True
+        self.show_dlls = True
         self.filter_changed_cells = True
         self.filter_interior_cells = False
-        self.filter_worldspaces = True
+        self.filter_worldspaces = False
         self.blacklist = blacklist()
 
         self.setStyleSheet("""
@@ -69,6 +80,18 @@ class list_compactable(QTableWidget):
     def create(self):
         self.setSortingEnabled(False)
         self.clearContents()
+        if not self.show_cells: 
+            self.hideColumn(self.CELL_COL)
+        else:
+            self.showColumn(self.CELL_COL)
+        if not self.show_dlls:
+            self.hideColumn(self.SKSE_COL)
+        else:
+            self.showColumn(self.SKSE_COL)
+        if self.filter_worldspaces: 
+            self.hideColumn(self.WRLD_COL)
+        else:
+            self.showColumn(self.WRLD_COL)
         self.dependency_list = self.get_data_from_file("ESLifier_Data/dependency_dictionary.json")
         self.compacted = self.get_data_from_file("ESLifier_Data/compacted_and_patched.json")
         self.dll_dict = self.get_data_from_file("ESLifier_Data/dll_dict.json")
@@ -76,14 +99,6 @@ class list_compactable(QTableWidget):
         self.cell_changed = self.get_data_from_file("ESLifier_Data/cell_changed.json")
         if blacklist == {}:
             blacklist = []
-
-        if self.filter_changed_cells:
-            if self.cell_changed == {}:
-                self.cell_changed = []
-            blacklist.extend(self.cell_changed)
-
-        if self.filter_interior_cells:
-            blacklist.extend([os.path.basename(plugin) for plugin, flags in self.flag_dict.items() if 'new_interior_cell' in flags])
 
         to_remove = []
         for mod in self.flag_dict:
@@ -98,10 +113,12 @@ class list_compactable(QTableWidget):
 
         def display_dependencies(mod_key):
             index = self.currentRow()
-            if self.cellWidget(index, 4):
-                self.item(index, 0).setTextAlignment(Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
-                if self.item(index, 1):
-                    self.item(index, 1).setTextAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+            if self.cellWidget(index, self.DEP_DISP_COL):
+                self.item(index, self.MOD_COL).setTextAlignment(Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
+                if self.item(index, self.CELL_COL):
+                    self.item(index, self.CELL_COL).setTextAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+                if self.item(index, self.WRLD_COL):
+                    self.item(index, self.WRLD_COL).setTextAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
                 self.sender().setText('Show')
                 self.sender().setStyleSheet("""
                     QPushButton{
@@ -109,11 +126,13 @@ class list_compactable(QTableWidget):
                         background-color: transparent;
                         border: none;
                     }""")
-                self.removeCellWidget(index, 4)
+                self.removeCellWidget(index, self.DEP_DISP_COL)
             else:
-                self.item(index, 0).setTextAlignment(Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
-                if self.item(index, 1):
-                    self.item(index, 1).setTextAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
+                self.item(index, self.MOD_COL).setTextAlignment(Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+                if self.item(index, self.CELL_COL):
+                    self.item(index, self.CELL_COL).setTextAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
+                if self.item(index, self.WRLD_COL):
+                    self.item(index, self.WRLD_COL).setTextAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
                 self.sender().setText('Hide')
                 self.sender().setStyleSheet("""
                     QPushButton{
@@ -129,7 +148,7 @@ class list_compactable(QTableWidget):
                     item.setToolTip(dependency)
                     list_widget_dependency_list.addItem(item)
                 list_widget_dependency_list.setSizeAdjustPolicy(QTableWidget.SizeAdjustPolicy.AdjustToContents)
-                self.setCellWidget(index, 4, list_widget_dependency_list)
+                self.setCellWidget(index, self.DEP_DISP_COL, list_widget_dependency_list)
             self.resizeRowToContents(index)
 
         for i, (plugin, flags) in enumerate(self.flag_dict.items()):
@@ -143,31 +162,46 @@ class list_compactable(QTableWidget):
                 item.setCheckState(Qt.CheckState.Unchecked)
             item.setToolTip(plugin)
             item.setTextAlignment(Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
-            self.setItem(i, 0, item)
+            self.setItem(i, self.MOD_COL, item)
             self.setRowHidden(i, False)
+            hide_row = False
             if 'new_cell' in flags:
                 item_cell_flag = QTableWidgetItem('New CELL')
                 item_cell_flag.setToolTip('This mod has a new CELL record and no mods currently modify it.\nIt is currently safe to ESL flag it.')
+                if not self.show_cells:
+                    hide_row = True
                 if basename in self.cell_changed:
                     item_cell_flag.setText('!!New CELL Changed!!')
                     item_cell_flag.setToolTip('This mod has at least one new CELL record that is an interior cell.\n'+
                                               'ESL interior cells do not reload properly on save game load until\n'+
                                               'the game has restarted.')
                     item_cell_flag.setToolTip('This mod has a new CELL record\nand has a dependent plugin that modifies it.\nIt is NOT recommended to ESL flag it.')
+                    if self.filter_changed_cells:
+                        hide_row = True
                 elif 'new_interior_cell' in flags:
                     item_cell_flag.setText('!New Interior CELL!')
                     item_cell_flag.setToolTip('This mod has at least one new CELL record that is an interior cell.\n'+
                                               'ESL created interior cells sometimes do not reload properly on a save\n'+
                                               'game load, until the game itself has restarted.')
+                    if self.filter_interior_cells:
+                        hide_row = True
                 item_cell_flag.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-                self.setItem(i, 1, item_cell_flag)
+                self.setItem(i, self.CELL_COL, item_cell_flag)
+            if 'new_wrld' in flags:
+                item_wrld_flag = QTableWidgetItem('!!New WRLD!!')
+                item_wrld_flag.setToolTip('This mod has a new WRLD (worldspace) record which may lose landscape (the ground) when ESL flagged.')
+                if self.filter_worldspaces:
+                    hide_row = True
+                self.setItem(i, self.WRLD_COL, item_wrld_flag)
             if basename.lower() in self.dll_dict:
-                item_dll = QTableWidgetItem('!SKSE DLL!')
+                item_dll = QTableWidgetItem('!!SKSE DLL!!')
                 tooltip = 'This mod\'s plugin name is present in the following SKSE dlls and\nmay break their FormLookup() function calls if a hard-coded form id is present:'
                 for dll in self.dll_dict[basename.lower()]:
                     tooltip += '\n- ' + os.path.basename(dll)
                 item_dll.setToolTip(tooltip)
-                self.setItem(i, 2, item_dll)
+                if not self.show_dlls:
+                    hide_row = True
+                self.setItem(i, self.SKSE_COL, item_dll)
             if self.dependency_list[basename.lower()] != []:
                 dL = QPushButton("Show")
                 dL.clicked.connect(lambda _, mod_key=basename.lower(): display_dependencies(mod_key))
@@ -181,8 +215,11 @@ class list_compactable(QTableWidget):
                     }""")
                 self.button_group.addButton(dL)
                 item_hidden = QTableWidgetItem('')
-                self.setCellWidget(i,3,dL)
-                self.setItem(i,3,item_hidden)
+                self.setCellWidget(i, self.DEP_COL,dL)
+                self.setItem(i, self.DEP_COL,item_hidden)
+
+            if hide_row:
+                self.setItem(i, self.HIDER_COL, QTableWidgetItem('hide me'))
 
         def somethingChanged(item_changed):
             self.blockSignals(True)
@@ -192,25 +229,23 @@ class list_compactable(QTableWidget):
             if multi_check:
                 if item_changed.checkState() == Qt.CheckState.Checked:
                     for x in self.selectedItems():
-                        if x.column() == 0:
+                        if x.column() == self.MOD_COL:
                             x.setCheckState(Qt.CheckState.Checked)
                 else:
                     for x in self.selectedItems():
-                        if x.column() == 0:
+                        if x.column() == self.MOD_COL:
                             x.setCheckState(Qt.CheckState.Unchecked)
             self.blockSignals(False)
-            
-        self.resizeColumnToContents(0)
-        self.resizeColumnToContents(1)
-        self.resizeColumnToContents(2)
-        self.resizeColumnToContents(3)
+
+        self.hide_rows()
+        self.resizeColumnsToContents()
         self.itemChanged.connect(somethingChanged)
         self.resizeRowsToContents()
         self.setSortingEnabled(True)
 
     def hide_rows(self):
         for row in range(self.rowCount()):
-            if self.item(row, 5):
+            if self.item(row, self.HIDER_COL):
                 self.setRowHidden(row, True)
             else:
                 self.setRowHidden(row, False)
@@ -227,13 +262,13 @@ class list_compactable(QTableWidget):
         row = self.rowAt(position.y())
         col = self.columnAt(position.x())
 
-        if col == 4 and self.cellWidget(row, 3) and self.cellWidget(row, 3).text() == 'Hide':
-            list_widget = self.cellWidget(row, 4)
+        if col == self.DEP_DISP_COL and self.cellWidget(row, self.DEP_COL) and self.cellWidget(row, self.DEP_COL).text() == 'Hide':
+            list_widget = self.cellWidget(row, self.DEP_DISP_COL)
             selected_item = list_widget.itemAt(list_widget.mapFromGlobal(self.viewport().mapToGlobal(position)))
             if not selected_item:
-                selected_item = self.item(row,0)
+                selected_item = self.item(row, self.MOD_COL)
         else:
-            selected_item = self.item(row, 0)
+            selected_item = self.item(row, self.MOD_COL)
 
         if selected_item:
             menu = QMenu(self)
@@ -265,15 +300,15 @@ class list_compactable(QTableWidget):
     def check_all(self):
         self.blockSignals(True)
         for i in range(self.rowCount()):
-            if not self.isRowHidden(i) and self.item(i,0).checkState() == Qt.CheckState.Unchecked:
-                self.item(i, 0).setCheckState(Qt.CheckState.Checked)
+            if not self.isRowHidden(i) and self.item(i,self.MOD_COL).checkState() == Qt.CheckState.Unchecked:
+                self.item(i, self.MOD_COL).setCheckState(Qt.CheckState.Checked)
         self.blockSignals(False)
 
     def uncheck_all(self):
         self.blockSignals(True)
         for i in range(self.rowCount()):
-            if self.item(i,0).checkState() == Qt.CheckState.Checked:
-                self.item(i, 0).setCheckState(Qt.CheckState.Unchecked)
+            if self.item(i,self.MOD_COL).checkState() == Qt.CheckState.Checked:
+                self.item(i, self.MOD_COL).setCheckState(Qt.CheckState.Unchecked)
         self.blockSignals(False)
     
     def select_all(self):
@@ -281,7 +316,7 @@ class list_compactable(QTableWidget):
         selection = QItemSelection()
         for row in range(self.rowCount()):
             if self.isRowHidden(row) == False:
-                selection.select(self.model().index(row, 0), self.model().index(row, self.model().columnCount() - 1))
+                selection.select(self.model().index(row, self.MOD_COL), self.model().index(row, self.model().columnCount() - 1))
         selection_model = self.selectionModel()
         selection_model.select(selection, selection_model.SelectionFlag.ClearAndSelect)
         self.blockSignals(False)
@@ -294,8 +329,8 @@ class list_compactable(QTableWidget):
                     previously_compacted = json.load(f)
                     f.close()
                 for row in range(self.rowCount()):
-                    if self.isRowHidden(row) == False and self.item(row, 0).checkState() == Qt.CheckState.Unchecked and self.item(row, 0).text() in previously_compacted:
-                        self.item(row,0).setCheckState(Qt.CheckState.Checked)
+                    if self.isRowHidden(row) == False and self.item(row, self.MOD_COL).checkState() == Qt.CheckState.Unchecked and self.item(row, self.MOD_COL).text() in previously_compacted:
+                        self.item(row, self.MOD_COL).setCheckState(Qt.CheckState.Checked)
             except Exception as e:
                 print('!Error: Failed to get previously_compacted.json')
                 print(e)
@@ -306,10 +341,10 @@ class list_compactable(QTableWidget):
         self.blockSignals(True)
         for item in selected_items:
             if item.checkState() == Qt.CheckState.Checked:
-                if item.column() == 0:
+                if item.column() == self.MOD_COL:
                     item.setCheckState(Qt.CheckState.Unchecked)
             elif item.checkState() == Qt.CheckState.Unchecked:
-                if item.column() == 0:
+                if item.column() == self.MOD_COL:
                     item.setCheckState(Qt.CheckState.Checked)
         self.blockSignals(False)
 
--- src/list_eslify.py
+++ src/list_eslify.py
@@ -9,19 +9,27 @@ from blacklist import blacklist
 
 class list_eslable(QTableWidget):
     def __init__(self):
+        self.COL_COUNT = 5
+        self.MOD_COL = 0
+        self.CELL_COL = 1
+        self.WRLD_COL = 2
+        self.SPACER_COL = 3
+        self.HIDER_COL = 4
         super().__init__()
-        self.setColumnCount(4)
-        self.setHorizontalHeaderLabels(['*   Mod', 'CELL Records', '', 'Hider'])
-        self.horizontalHeaderItem(0).setToolTip('This is the plugin name. Select which plugins you wish to flag as light.')
-        self.horizontalHeaderItem(1).setToolTip('This is the CELL Record Flag. If an ESL plugin creates a new CELL\n'+
+        self.setColumnCount(self.COL_COUNT)
+        self.setHorizontalHeaderLabels(['*   Mod', 'CELL Records', 'WRLD Records', '', 'Hider'])
+        self.horizontalHeaderItem(self.MOD_COL).setToolTip('This is the plugin name. Select which plugins you wish to flag as light.')
+        self.horizontalHeaderItem(self.CELL_COL).setToolTip('This is the CELL Record Flag. If an ESL plugin creates a new CELL\n'+
                                                 'and another mod changes that CELL then it may not work due to an engine bug.\n'+
                                                 'If an ESL plugin creates a new interior CELL then that cell may experience\n'+
                                                 'issues when reloading a save without restarting the game.\n'+
                                                 '"New  CELL" indicates the presence of a new CELL record.\n'+
                                                 '"!New Interior CELL!" indicates that a new CELL is an interior.\n'+
                                                 '"!!New CELL Changed!!" indicates that a new CELL record is changed by a dependent plugin.')
-        self.setColumnHidden(3, True)
-        self.horizontalHeader().sortIndicatorChanged.connect(self.rehide_rows)
+        self.horizontalHeaderItem(self.WRLD_COL).setToolTip('This is the WRLD Record Flag. If an plugin is flagged ESL\n'+
+                                                       'then the new worldspace may have landscape issues (no ground).')
+        self.setColumnHidden(self.HIDER_COL, True)
+        self.horizontalHeader().sortIndicatorChanged.connect(self.hide_rows)
         self.verticalHeader().setHidden(True)
         self.setShowGrid(False)
         self.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
@@ -33,10 +41,10 @@ class list_eslable(QTableWidget):
         self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
         self.customContextMenuRequested.connect(self.contextMenu)
         self.flag_dict = {}
+        self.show_cells = True
         self.filter_changed_cells = True
         self.filter_interior_cells = False
-        self.filter_world_spacer = True
-
+        self.filter_worldspaces = False
         self.blacklist = blacklist()
 
         self.setStyleSheet("""
@@ -64,19 +72,19 @@ class list_eslable(QTableWidget):
     def create(self):
         self.setSortingEnabled(False)
         self.clearContents()
+        if not self.show_cells:
+            self.hideColumn(self.CELL_COL)
+        else:
+            self.showColumn(self.CELL_COL)
+        if self.filter_worldspaces:
+            self.hideColumn(self.WRLD_COL)
+        else:
+            self.showColumn(self.WRLD_COL)
         self.compacted = self.get_data_from_file("ESLifier_Data/compacted_and_patched.json")
         blacklist = self.get_data_from_file('ESLifier_Data/blacklist.json')
         self.cell_changed = self.get_data_from_file("ESLifier_Data/cell_changed.json")
         if blacklist == {}:
             blacklist = []
-
-        if self.filter_changed_cells:
-            if self.cell_changed == {}:
-                self.cell_changed = []
-            blacklist.extend(self.cell_changed)
-
-        if self.filter_interior_cells:
-            blacklist.extend([os.path.basename(plugin) for plugin, flags in self.flag_dict.items() if 'new_interior_cell' in flags])
 
         to_remove = []
         for mod in self.flag_dict:
@@ -94,22 +102,36 @@ class list_eslable(QTableWidget):
             item.setToolTip(plugin)
             item.setFlags(item.flags() | Qt.ItemFlag.ItemIsUserCheckable)
             item.setCheckState(Qt.CheckState.Unchecked)
-            self.setItem(i, 0, item)
+            self.setItem(i, self.MOD_COL, item)
             self.setRowHidden(i, False)
-            
+            hide_row = False
             if 'new_cell' in flags:
                 item_cell_flag = QTableWidgetItem('New CELL')
                 item_cell_flag.setToolTip('This mod has a new CELL record.')
+                if not self.show_cells:
+                    hide_row = True
                 if basename in self.cell_changed:
                     item_cell_flag.setText('!!New CELL Changed!!')
                     item_cell_flag.setToolTip('This mod has a new CELL record\nand has a dependent plugin that modifies it.\nIt is NOT recommended to esl it.')
+                    if self.filter_changed_cells:
+                        hide_row = True
                 elif 'new_interior_cell' in flags:
                     item_cell_flag.setText('!New Interior CELL!')
                     item_cell_flag.setToolTip('This mod has at least one new CELL record that is an interior cell.\n'+
                                               'ESL created interior cells sometimes do not reload properly on a save\n'+
                                               'game load, until the game itself has restarted.')
+                    if self.filter_interior_cells:
+                        hide_row = True
                 item_cell_flag.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-                self.setItem(i, 1, item_cell_flag)
+                self.setItem(i, self.CELL_COL, item_cell_flag)
+            if 'new_wrld' in flags:
+                item_wrld_flag = QTableWidgetItem('!!New WRLD!!')
+                item_wrld_flag.setToolTip('This mod has a new WRLD (worldspace) record which may lose landscape (the ground) when ESL flagged.')
+                if self.filter_worldspaces:
+                    hide_row = True
+                self.setItem(i, self.WRLD_COL, item_wrld_flag)
+            if hide_row:
+                self.setItem(i, self.HIDER_COL, QTableWidgetItem('hide me'))
 
         def somethingChanged(itemChanged):
             self.blockSignals(True)
@@ -119,23 +141,23 @@ class list_eslable(QTableWidget):
             if multi_check:
                 if itemChanged.checkState() == Qt.CheckState.Checked:
                     for x in self.selectedItems():
-                        if x.column() == 0:
+                        if x.column() == self.MOD_COL:
                             x.setCheckState(Qt.CheckState.Checked)
                 else:
                     for x in self.selectedItems():
-                        if x.column() == 0:
+                        if x.column() == self.MOD_COL:
                             x.setCheckState(Qt.CheckState.Unchecked)
             self.blockSignals(False)
 
-        self.resizeColumnToContents(0)
-        self.resizeColumnToContents(1)
+        self.hide_rows()
+        self.resizeColumnsToContents()
         self.itemChanged.connect(somethingChanged)
         self.resizeRowsToContents()
         self.setSortingEnabled(True)
 
-    def rehide_rows(self):
+    def hide_rows(self):
         for row in range(self.rowCount()):
-            if self.item(row, 3):
+            if self.item(row, self.HIDER_COL):
                 self.setRowHidden(row, True)
             else:
                 self.setRowHidden(row, False)
@@ -177,15 +199,15 @@ class list_eslable(QTableWidget):
     def check_all(self):
         self.blockSignals(True)
         for i in range(self.rowCount()):
-            if not self.isRowHidden(i) and self.item(i,0).checkState() == Qt.CheckState.Unchecked:
-                self.item(i, 0).setCheckState(Qt.CheckState.Checked)
+            if not self.isRowHidden(i) and self.item(i, self.MOD_COL).checkState() == Qt.CheckState.Unchecked:
+                self.item(i, self.MOD_COL).setCheckState(Qt.CheckState.Checked)
         self.blockSignals(False)
 
     def uncheck_all(self):
         self.blockSignals(True)
         for i in range(self.rowCount()):
-            if self.item(i,0).checkState() == Qt.CheckState.Checked:
-                self.item(i, 0).setCheckState(Qt.CheckState.Unchecked)
+            if self.item(i, self.MOD_COL).checkState() == Qt.CheckState.Checked:
+                self.item(i, self.MOD_COL).setCheckState(Qt.CheckState.Unchecked)
         self.blockSignals(False)
 
     def select_all(self):
@@ -193,7 +215,7 @@ class list_eslable(QTableWidget):
         selection = QItemSelection()
         for row in range(self.rowCount()):
             if self.isRowHidden(row) == False:
-                selection.select(self.model().index(row, 0), self.model().index(row, self.model().columnCount() - 1))
+                selection.select(self.model().index(row, self.MOD_COL), self.model().index(row, self.model().columnCount() - 1))
         selection_model = self.selectionModel()
         selection_model.select(selection, selection_model.SelectionFlag.ClearAndSelect)
         self.blockSignals(False)
@@ -202,10 +224,10 @@ class list_eslable(QTableWidget):
         self.blockSignals(True)
         for item in selected_items:
             if item.checkState() == Qt.CheckState.Checked:
-                if item.column() == 0:
+                if item.column() == self.MOD_COL:
                     item.setCheckState(Qt.CheckState.Unchecked)
             elif item.checkState() == Qt.CheckState.Unchecked:
-                if item.column() == 0:
+                if item.column() == self.MOD_COL:
                     item.setCheckState(Qt.CheckState.Checked)
         self.blockSignals(False)
 
@@ -225,6 +247,6 @@ class list_eslable(QTableWidget):
                 print(f"Error opening file explorer: {e}")
 
     def add_to_blacklist(self, selected_items):
-        mods = [item.text() for item in selected_items if item.column() == 0]
+        mods = [item.text() for item in selected_items if item.column() == self.MOD_COL]
         self.blacklist.add_to_blacklist(mods)
         self.create()
