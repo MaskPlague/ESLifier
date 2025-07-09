971019c4b1a09167a8afb1146d0a1d5a0aed7d8e
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-07-09 06:37:24 -0500

    remove eslifier notifier from mo2 plugins

--- src/ESLifier MO2 Integration Plugin/__init__.py
+++ src/ESLifier MO2 Integration Plugin/__init__.py
@@ -6,7 +6,6 @@ site.addsitedir(os.path.join(os.path.dirname(__file__), "lib"))
 
 from mobase import IPlugin
 from .ESLifier import ESLifier
-from .ESLifier_notifier import ESLifier_Notifier
 
 def createPlugins() -> List["IPlugin"]:
-    return [ESLifier(), ESLifier_Notifier()]
+    return [ESLifier()]
