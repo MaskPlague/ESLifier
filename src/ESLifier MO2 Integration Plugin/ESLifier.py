ab3f144a089e8caf815ff7347e32cdc79c23c767
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-07-09 23:18:50 -0500

    Add plugin setting dialog

--- src/ESLifier MO2 Integration Plugin/ESLifier.py
+++ src/ESLifier MO2 Integration Plugin/ESLifier.py
@@ -20,7 +20,24 @@ class ESLifier(mobase.IPluginTool):
     
     def init(self, organiser = mobase.IOrganizer):
         self._organizer = organiser
-        self.dialog = QDialog()
+        self.main_dialog = QDialog()
+        self.settings_dialog = QDialog()
+        self.thread = QThread()
+        self.check_again = False
+        self.needs_flag_dict = {}
+        self.needs_compacting_flag_dict = {}
+        self.hash_mismatches = []
+
+        icon_path = os.path.join(os.path.split(self._organizer.getPluginDataPath())[0], 'ESLifier MO2 Integration')
+        self.eslifier_icon = QIcon(icon_path + '\\ESLifier.ico')
+        self.main_dialog.setWindowIcon(self.eslifier_icon)
+        self.eslifier_icon_notif = QIcon(icon_path + '\\ESLifier_with_notif_badge.ico')
+        self.eslifier_icon_greyed_out = QIcon(icon_path + '\\ESLifier_greyed_out.ico')
+
+        self.eslifier_button = QToolButton()
+        self.eslifier_button.setIcon(self.eslifier_icon_greyed_out)
+        self.eslifier_button.clicked.connect(self.display)
+
         self._organizer.onUserInterfaceInitialized(self.create)
         self.icon_path = os.path.join(os.path.split(self._organizer.getPluginDataPath())[0], 'ESLifier MO2 Integration')
         self.notifier = ESLifier_Notifier()
@@ -55,6 +72,43 @@ class ESLifier(mobase.IPluginTool):
             mobase.PluginSetting("ESLifier Folder", self.tr("Set this to the folder that holds ESLifier.exe."), ""),
         ]
     
+    def create_label_check_box_setting_pair(self, layout: QGridLayout, text: str, row: int) -> QCheckBox:
+        label = QLabel(text)
+        check_box = QCheckBox()
+        def changed():
+            self._organizer.setPluginSetting(self.name(), text, check_box.isChecked())
+        check_box.checkStateChanged.connect(changed)
+        layout.addWidget(label, row, 0)
+        layout.addWidget(check_box, row, 1)
+        return check_box
+    
+    def create_settings_dialog(self, _):
+        self.settings_dialog = QDialog()
+        layout = QGridLayout()
+        self.settings_dialog.setLayout(layout)
+
+        self.notifications_check_box = self.create_label_check_box_setting_pair(layout, "Enable Notifications", 0)
+        self.esms_check_box = self.create_label_check_box_setting_pair(layout, "scan_esms", 1)
+        self.header_check_box = self.create_label_check_box_setting_pair(layout, "Use 1.71 Header Range", 2)
+
+        folder = self._organizer.pluginSetting(self.name(), "ESLifier Folder")
+        if len(folder) > 30:
+            self.folder_path = QLabel(f"...{folder[-30:]}")
+        else:
+            self.folder_path = QLabel(folder)
+        self.folder_path.setToolTip("Set this to the folder that holds ESLifier.exe")
+        layout.addWidget(self.folder_path, 3, 0)
+        path_button = self.button_maker("Explore", self.set_eslifier_path)
+        path_button.setToolTip("Set this to the folder that holds ESLifier.exe")
+        layout.addWidget(path_button, 3, 1)
+
+        done_button = QPushButton("Done")
+        def done():
+            self.settings_dialog.hide()
+            self._organizer.refresh()
+        done_button.clicked.connect(done)
+        layout.addWidget(done_button, 4, 0)
+
     def displayName(self):
         return self.tr("ESLifier")
     
@@ -62,7 +116,7 @@ class ESLifier(mobase.IPluginTool):
         return self.tr("")
     
     def icon(self):
-        return QIcon(self.icon_path + '\\ESLifier.ico')
+        return self.eslifier_icon
 
     def getFiles(self):
         return
@@ -137,12 +191,49 @@ class ESLifier(mobase.IPluginTool):
     def button_maker(self, name, function, hide=False):
         button = QPushButton(name)
         if hide:
-            button.clicked.connect(self.dialog.hide)
+            button.clicked.connect(self.main_dialog.hide)
         if function:
             button.clicked.connect(function)
         return button
 
     def display(self):
-        self.dialog.raise_()
-        self.dialog.show()
+        self.main_dialog.raise_()
+        self.main_dialog.show()
         return
+    
+    def display_settings(self):
+        notifications = self._organizer.pluginSetting(self.name(), "Enable Notifications")
+        self.notifications_check_box.setChecked(notifications)
+
+        esms = self._organizer.pluginSetting(self.name(), "Scan ESMs")
+        self.esms_check_box.setChecked(esms)
+
+        header = self._organizer.pluginSetting(self.name(), "Use 1.71 Header Range")
+        self.header_check_box.setChecked(header)
+
+        self.settings_dialog.raise_()
+        self.settings_dialog.show()
+        return
+    
+    def display_notifications(self):
+        self.notifcation_display.create(self.hash_mismatches, self.needs_flag_dict, self.needs_compacting_flag_dict)
+
+
+class CheckWorker(QObject):
+    finished_signal = pyqtSignal(bool, dict, dict, list)
+    def __init__(self, eslifier: ESLifier, plugin_checker: check_plugins, scan_esms, eslifier_folder, new_header, compare_hashes, ignore_list, filter):
+        super().__init__()
+        self.eslifier = eslifier
+        self.plugin_checker = plugin_checker
+        self.scan_esms = scan_esms
+        self.eslifier_folder = eslifier_folder
+        self.new_header = new_header
+        self.compare_hashes = compare_hashes
+        self.ignore_list = ignore_list
+        self.filter = filter
+
+    def check_for_esl(self):
+        plugin_files_list = [plugin for plugin in self.eslifier._organizer.findFiles('', self.filter) if os.path.basename(plugin) not in self.ignore_list]
+        flag, needs_flag_dict, needs_compacting_flag_dict, hash_mismatches = self.plugin_checker.scan_for_eslable(self.scan_esms, self.eslifier_folder, self.new_header, 
+                                                                                                 self.compare_hashes, plugin_files_list)
+        self.finished_signal.emit(flag, needs_flag_dict, needs_compacting_flag_dict, hash_mismatches)
