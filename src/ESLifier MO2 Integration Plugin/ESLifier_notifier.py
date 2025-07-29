e7327609da15cbe140414d0d38d626f23e742657
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-07-29 03:11:42 -0500

    add early exit method, add sortting

--- src/ESLifier MO2 Integration Plugin/ESLifier_notifier.py
+++ src/ESLifier MO2 Integration Plugin/ESLifier_notifier.py
@@ -18,6 +18,7 @@ class check_files():
         self.hash_mismatches.clear()
         self.any_esl = False
         self.eslifier = eslifier
+        self.running = True
 
         if len(plugin_files_list) > 1000:
             split = 5
@@ -51,55 +52,64 @@ class check_files():
         self.conflict_changes = []
         if detect_conflict_changes:
             winning_file_history_dict_path = os.path.join(eslifier_folder, "ESLifier_Data/winning_file_history_dict.json")
+            mod_list = self.eslifier._organizer.modList().allModsByProfilePriority()
             if os.path.exists(winning_file_history_dict_path):
                 with self.semaphore:
                     with open(winning_file_history_dict_path, 'r', encoding='utf-8') as f:
                         winning_file_history_dict: dict[str, list[str]] = json.load(f)
                 for file, old_winner in winning_file_history_dict.items():
-                    if not 'bsa_extracted_eslifier_scan' == old_winner:
-                        thread = threading.Thread(target=self.detect_conflict_change, args=(old_winner, file))
-                        threads.append(thread)
-                        thread.start()
-
-        for thread in threads:
-            thread.join()
-
-        threads.clear()
+                    if self.running:
+                        if not 'bsa_extracted_eslifier_scan' == old_winner:
+                            self.detect_conflict_change(old_winner, file, mod_list)
+                    else:
+                        break
 
         if compare_hashes and os.path.exists(original_plugins_path):
             for plugin, original_hash in original_plugins_hash_map:
-                thread = threading.Thread(target=self.compare_previous_hash_to_current, args=(plugin, original_hash))
-                threads.append(thread)
-                thread.start()
+                if self.running:
+                    self.compare_previous_hash_to_current(plugin, original_hash)
+                else:
+                    break
 
         for thread in threads:
             thread.join()
 
         needs_flag_dict = {p: f for p, f in self.flag_dict.items() if 'need_compacting' not in f}
         needs_compacting_flag_dict = {p: f for p, f in self.flag_dict.items() if 'need_compacting' in f}
-        return ((self.any_esl or len(self.hash_mismatches) > 0 or len(self.conflict_changes) > 0), 
+        if self.running:
+            return ((self.any_esl or len(self.hash_mismatches) > 0 or len(self.conflict_changes) > 0), 
                 needs_flag_dict, needs_compacting_flag_dict, self.hash_mismatches, self.conflict_changes)
+        else:
+            return False, {}, {}, [], []
+
+    def stop(self):
+        self.running = False
     
     def compare_previous_hash_to_current(self, file, original_hash):
         if os.path.exists(file):
             with self.semaphore:
-                with open(file, 'rb') as f:
-                    data = f.read()
-            if hashlib.sha256(data).hexdigest() != original_hash:
-                with self.lock:
-                    self.hash_mismatches.append("Hash Mismatch: " + file)
+                if self.running:
+                    with open(file, 'rb') as f:
+                        data = f.read()
+            if self.running and hashlib.sha256(data).hexdigest() != original_hash:
+                self.hash_mismatches.append("Hash Mismatch: " + file)
         else:
-            with self.lock:
-                self.hash_mismatches.append("Missing: " + file)
+            self.hash_mismatches.append("Missing: " + file)
     
-    def detect_conflict_change(self, old_winner, file):
+    def detect_conflict_change(self, old_winner, file, mod_list: list):
         origins: list = self.eslifier._organizer.getFileOrigins(file)
         if len(origins) == 1:
             if old_winner != origins[0]:
                 self.conflict_changes.append(file)
         elif len(origins) > 0:
-            if origins[-1] != old_winner:
-                self.conflict_changes.append(file)
+            sorted_origins = sorted(origins, key=lambda x: mod_list.index(x))
+            if 'eslifier' in sorted_origins[-1].lower():
+                if sorted_origins[-2] != old_winner:
+                    self.conflict_changes.append(file)
+            else:
+                if sorted_origins[-1] != old_winner:
+                    self.conflict_changes.append(file)
+
 
     def plugin_scanner(self, plugins, new_header, scan_esms):
         flag_dict = {}
