6aabce0bcd8e8532411fa4625787566a2a2be16c
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-07-21 02:45:22 -0500

    Fix: stripping purposefully included whitespace

--- src/file_defined_patcher_conditions.py
+++ src/file_defined_patcher_conditions.py
@@ -66,9 +66,9 @@ class user_and_master_conditions_class():
             try:
                 extension = conditions["extension"].lower()
                 conditions_dict.get(extension, conditions_dict["other"]).append({
-                    "contains": os.path.normpath(conditions.get("contains", "").lower().strip()),
-                    "endswith": os.path.normpath(conditions.get("endswith", "").lower().strip()),
-                    "separator": conditions.get("separator", "").lower().strip(),
+                    "contains": os.path.normpath(conditions.get("contains", "").lower()),
+                    "endswith": os.path.normpath(conditions.get("endswith", "").lower()),
+                    "separator": conditions.get("separator", "").lower(),
                     "int_type": conditions.get("int_type", False),
                     "tkns": set(conditions.get("tokens", {" "})),
                     "fid_start": conditions.get("fid_start"),
@@ -126,7 +126,7 @@ class user_and_master_conditions_class():
 
 #if __name__ == "__main__":
 #    t = user_and_master_conditions_class()
-#    print(t.user_and_master_conditions['toml'])
+#    print(t.user_and_master_conditions['ini'])
 #    form_id_map = {int('0x000A2C8E',16): {'hex_no_0': 'A0A', 'hex': '000A0A', 'int': 10, 'bytes': b'\x00\x0A\x0A', 'update_name': False}, 
 #                   int('0x0001A696',16): {'hex_no_0': 'B0B', 'hex': '000B0B', "int": 10101, 'bytes': b'\x00\x0B\x0B', 'update_name': False}}
 #    file = os.path.normpath(r)
