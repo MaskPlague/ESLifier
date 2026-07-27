45dc0ff3b3b6523e046200f89e0436aed8cef961
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-07-27 05:46:36 -0500

    ignore Nexus collection.json
    Ignored here specifically just in case the file meets a patching condition and isn't a Nexus collection json file but just something with the same name.

--- src/patcher_conditions.py
+++ src/patcher_conditions.py
@@ -159,7 +159,7 @@ def patch_file_conditions(new_file_lower: str, new_file: str, basename: str, for
             patchers.json_dyndolod_ligh_patcher(basename, new_file, form_id_map, master_byte, encoding_method=encoding)
         else:
             patched = additional_conditions.check_conditions(basename, new_file, new_file_lower, form_id_map)
-            if not patched:                                           
+            if not patched and not new_file_lower.endswith("\\collection.json"):                                           
                 write_warning(QCoreApplication.translate("patcher_conditions", "Possible missing patcher for: ") + new_file)
     elif new_file_lower.endswith('.pex'):                                                   # Compiled script patching
         patchers.pex_patcher(basename, new_file, form_id_map)
