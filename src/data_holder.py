1aeabec83bce3ed7b4dfb00f3529d9bbc1bd7afa
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-07-27 05:43:22 -0500

    create shared global module

---
+++ src/data_holder.py
@@ -0,0 +1,75 @@
+from settings_page import settings
+import os
+
+class _global():
+    # Commonly Accessed Settings
+    _settings: dict = {}
+    skyrim_folder_path = ''
+    output_folder_path = ''
+    output_folder_name = ''
+    output_folder_joined_path = ''
+    overwrite_path = ''
+    plugins_txt_path = ''
+    mo2_modlist_txt_path = ''
+    vortex_data_path = ''
+    mod_manager_mode = 0
+    update_header = True
+    generate_cell_master = False
+    persistent_ids = True
+    free_non_existent = False
+    hash_output = True
+    all_patcher_experimental = False
+    mod_staging_folder = ''
+
+    # Non-Persistent Variables
+    plugins = [] #plugins list
+    mods_with_seq = []
+    vortex_error = -1
+
+
+    def init(settings_widget: settings):
+        _global._settings = settings_widget.settings
+
+    def update_from_settings():
+        _global.skyrim_folder_path =          _global._settings.get('skyrim_folder_path', '')
+        _global.output_folder_path =          _global._settings.get('output_folder_path', '')
+        _global.output_folder_name =          _global._settings.get('output_folder_name', "ESLifier Output")
+        _global.mod_manager_mode =            _global._settings.get('mod_manager_mode', 0)
+        _global.mo2_modlist_txt_path =        _global._settings.get('mo2_modlist_txt_path', '')
+        _global.vortex_data_path =            _global._settings.get('vortex_data_path', '')
+        _global.plugins_txt_path =            _global._settings.get('plugins_txt_path', '')
+        _global.overwrite_path =              _global._settings.get('overwrite_path', '')
+        _global.update_header =               _global._settings.get('update_header', True)
+        _global.generate_cell_master =        _global._settings.get('generate_cell_master', True)
+        _global.persistent_ids =              _global._settings.get('persistent_ids', True)
+        _global.free_non_existent =           _global._settings.get('free_non_existent', False)
+        _global.hash_output =                 _global._settings.get('hash_output', True)
+        _global.all_patcher_experimental =    _global._settings.get('all_patcher_experimental', False)
+        _global.output_folder_joined_path =   os.path.normpath(os.path.join(_global.output_folder_path, _global.output_folder_name))
+
+    def get_rel_path(file: str) -> str:
+        if 'bsa_extracted' in file:
+            if 'bsa_extracted_temp' in file:
+                start = os.path.join(os.getcwd(), 'bsa_extracted_temp/')
+            else:
+                start = os.path.join(os.getcwd(), 'bsa_extracted/')
+            rel_path = os.path.normpath(os.path.relpath(file, start))
+        elif _global.mod_manager_mode == 2 and file.lower().startswith(_global.overwrite_path.lower()):
+            rel_path = os.path.normpath(os.path.relpath(file, _global.overwrite_path))
+        else:
+            if _global.mod_manager_mode == 2:
+                parts = os.path.normpath(os.path.relpath(file, _global.skyrim_folder_path)).split(os.sep)
+                if len(parts) != 1:
+                    parts = parts[1:]
+                rel_path = os.path.join(*parts)
+            elif _global.mod_manager_mode == 1:
+                if file.startswith(_global.mod_staging_folder):
+                    parts = os.path.normpath(os.path.relpath(file, _global.mod_staging_folder)).split(os.sep)
+                else:
+                    parts = os.path.normpath(os.path.relpath(file, _global.skyrim_folder_path)).split(os.sep)
+                if len(parts) != 1:
+                    parts = parts[1:]
+                rel_path = os.path.join(*parts)
+            else:
+                rel_path = os.path.normpath(os.path.relpath(file, _global.skyrim_folder_path))
+        return rel_path
