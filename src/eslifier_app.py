d8fdbfc4c3d2c865c3cf46809e9692551469daf8
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-07-29 00:28:18 -0500

    Update eslifier_app.py

--- src/eslifier_app.py
+++ src/eslifier_app.py
@@ -563,7 +563,7 @@ class main_window(QMainWindow):
 
 try:
     app = QApplication(sys.argv)
-
+    #app.setStyle('Fusion') # Not setting this but leaving it here just in case I need to
     translator = QTranslator()
     lang_file = os.path.normpath("ESLifier_Data/translation.qm")
     if os.path.exists(lang_file):
