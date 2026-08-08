fb691381b063407daa0e858e3adeb500eec1e16e
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-08-08 03:30:08 -0500

    try building pyinstaller locally

--- ready_environment.bat
+++ ready_environment.bat
@@ -6,4 +6,9 @@ call conda install -y leveldb
 
 call conda install -y --file requirements.txt --channel conda-forge
 
+rem call pip install PySide6
+
 call pip install PyQt6-stubs
+
+call set PYINSTALLER_COMPILE_BOOTLOADER=1
+call pip install --verbose --no-binary=PyInstaller PyInstaller
