59c0437adfb5b052aee6ca9cdf2ffbe549210c15
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-09-05 03:21:33 -0500

    Update eslifier_app.py

--- src/eslifier_app.py
+++ src/eslifier_app.py
@@ -18,7 +18,7 @@ from data_holder import _global
 from scanner_vortex import Vortex
 from scanner_mo2 import MO2
 
-CURRENT_VERSION = '0.16.15'
+CURRENT_VERSION = '0.16.16'
 MAJOR, MINOR, PATCH = [int(x, 10) for x in CURRENT_VERSION.split('.')] 
 VERSION_TUPLE = (MAJOR, MINOR, PATCH)
 
